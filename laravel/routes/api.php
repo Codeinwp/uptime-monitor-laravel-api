8d5fbb4f5099a9db78858ee7a621974841c02a40
Bogdan Preda <bogdan.preda@themeisle.com>
2018-01-22 15:10:23 +0200

    WIP: Updated routes

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:api');
 
 Route::get('/monitor', function() {
 	// If the Content-Type and Accept headers are set to 'application/json',
@@ -26,14 +26,27 @@ Route::get('/monitor', function() {
 });
 
 
-Route::get('/monitor/create', function() {
+Route::post('/monitor/create', function() {
 	// If the Content-Type and Accept headers are set to 'application/json',
 	// this will return a JSON structure. This will be cleaned up later.
 
-	$url = $_GET['url'];
+	$url = ( isset( $_POST['url'] ) ) ? $_POST['url'] : '';
+	$email = ( isset( $_POST['email'] ) ) ? $_POST['email'] : '';
 
 	$decoded_url = urldecode ( $url );
 
-	$exitCode = Artisan::call( "monitor:create", [ 'url' => $decoded_url, '--api' => true ] );
+	$exitCode = Artisan::call( "monitor:create", [ 'url' => $decoded_url, '--api' => true, '--email' => $email ] );
+	return Artisan::output();
+});
+
+Route::post('/monitor/remove', function() {
+	// If the Content-Type and Accept headers are set to 'application/json',
+	// this will return a JSON structure. This will be cleaned up later.
+
+	$url = ( isset( $_POST['url'] ) ) ? $_POST['url'] : '';
+
+	$decoded_url = urldecode ( $url );
+
+	$exitCode = Artisan::call( "monitor:delete", [ 'url' => $decoded_url, '--api' => true ] );
 	return Artisan::output();
 });
